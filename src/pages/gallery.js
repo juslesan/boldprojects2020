79703679a822ea2e702087310a48a1a5dc180511
juslesan <santeri.juslenius@gmail.com>
2020-01-27 15:50:06 +0200

    Gallery fade

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -12,6 +12,7 @@ import img6 from '../img/gallery/rsz__dsf1101.jpg'
 import img7 from '../img/bold100test/DSC06425.jpg'
 import img8 from '../img/gallery/rsz_dos_men_1.jpg'
 import img9 from '../img/gallery/nätram.jpg'
+import Fade from 'react-reveal/Fade';
 
 export default class Contact extends React.Component {
   constructor(props) {
@@ -44,50 +45,68 @@ export default class Contact extends React.Component {
           </div>
           <p className="galleryImgText">Bold Projects</p>
         </Grid>
-
+        
         <Grid item xs={12} sm={6}>
-          <img src={img1} alt="img1" className="gallery1Img"/>
-          <p className="galleryImgText"> Bold Voyage x Notes Magazine</p>
+          <Fade>
+            <img src={img1} alt="img1" className="gallery1Img"/>
+            <p className="galleryImgText"> Bold Voyage x Notes Magazine</p>
+          </Fade>
         </Grid>
   
         <Grid item xs={12} sm={6}>
-          <img src={img4} alt="img2"className="gallery2Img"/>
-          <p className="galleryImgText">Bold Voyage x Annette Tillander</p>
+          <Fade>
+            <img src={img4} alt="img2"className="gallery2Img"/>
+            <p className="galleryImgText">Bold Voyage x Annette Tillander</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6} className="gallery3">
-          <img src={img6} alt="img3" className="gallery3Img"/>
-          <p className="galleryImgText">Bold Voyage x Fazer</p>
+          <Fade>
+            <img src={img6} alt="img3" className="gallery3Img"/>
+            <p className="galleryImgText">Bold Voyage x Fazer</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6}>
-          <img src={img8} alt="img4" className="gallery2Img"/>
-          <p className="galleryImgText">Bold Voyage x Finnlines</p>
+          <Fade>
+            <img src={img8} alt="img4" className="gallery2Img"/>
+            <p className="galleryImgText">Bold Voyage x Finnlines</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6} className="gallery3">
-          <img src={img7} alt="img5" className="gallery3Img"/>
-          <p className="galleryImgText">BOLD 100´19</p>
+          <Fade>
+            <img src={img7} alt="img5" className="gallery3Img"/>
+            <p className="galleryImgText">BOLD 100´19</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6} className="gallery3">
-          <img src={img9} alt="img6" className="gallery2Img"/>
-          <p className="galleryImgText">Eco friendly boating</p>
+          <Fade>
+            <img src={img9} alt="img6" className="gallery2Img"/>
+            <p className="galleryImgText">Eco friendly boating</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={12} className="gallery4">
-          <img src={img2} alt="img7" className="gallery4Img"/>
-          <p className="galleryImgText">Last to appear, first to go</p>
+          <Fade>
+            <img src={img2} alt="img7" className="gallery4Img"/>
+            <p className="galleryImgText">Last to appear, first to go</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6} className="gallery3">
-          <img src={img3} alt="img8" className="gallery3Img"/>
-          <p className="galleryImgText">Modern farming takes the farming into account</p>
+          <Fade>
+            <img src={img3} alt="img8" className="gallery3Img"/>
+            <p className="galleryImgText">Modern farming takes the farming into account</p>
+          </Fade>
         </Grid>
 
         <Grid item xs={12} sm={6} className="gallery3">
-          <img src={img5} alt="img9" className="gallery4Img"/>
-          <p className="galleryImgText">Lafevest</p>
+          <Fade>
+            <img src={img5} alt="img9" className="gallery4Img"/>
+            <p className="galleryImgText">Lafevest</p>
+          </Fade>
         </Grid>
       </Grid>
     )
