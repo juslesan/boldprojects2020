927030af5e91aaf57df35638d074839d5d05406f
juslesan <santeri.juslenius@gmail.com>
2020-11-29 21:14:49 +0200

    styling

--- src/pages/portfolioRoll/roller.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ImageRight from './imageRight'
-
-export default function roller(imgs, text, popup) {
-    return(
-      <ImageRight text={text} imgs={imgs} popup={popup}/>
-    )
-  
-}
