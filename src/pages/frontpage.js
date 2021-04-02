0307d8ad99c0716629da6522bf069ebaee8a6782
juslesan <santeri.juslenius@gmail.com>
2021-04-02 17:33:11 +0300

    project 2021 page

--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import boldgif from '../img/image00002.gif'
-
+import projectLink from '../img/project2021/PP_button.png'
 export default class Frontpage extends React.Component {
   constructor(props) {
     super(props)
@@ -20,6 +20,9 @@ export default class Frontpage extends React.Component {
         <Grid container spacing={10} alignItems="center" justify="center" className="frontcont">
           <Grid item xs={12} sm={6}>
               <img className="frontGif" src={boldgif}></img>
+              <a href="/project2021" className="mieliLink">
+                <img src={projectLink} className="mieliLinkImg"/>
+              </a>
           </Grid>
           <Grid item xs={12} sm={6}>
             <h1 className="frontFont">Kaikilla on oikeus hyvään elämään.</h1>
--- src/pages/project2021.js
+++ src/pages/project2021.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import content from '../content/project2021'
 import Finland from '../img/project2021/Finland.png'
+import Donate from '../img/project2021/Donate_button.png'
 export default class Project2020 extends React.Component {
   constructor(props) {
     super(props)
@@ -18,13 +19,20 @@ export default class Project2020 extends React.Component {
           <Grid item xs={12} className="pageHeader">
             <h1 className="portfolioHeader">Päästä Päähän</h1>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={10}>
             <div className="project2021Texts">
               <p className="project2021text">{this.props.text[0]}</p>
             </div>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <img src={Finland} className="trailImg"/>
+          <Grid item xs={12} sm={2}>
+            <div className="mapImgWrapper">
+              <img src={Finland} className="mapImg"/>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <a href="" className="donateLink">
+                <img src={Donate} className="donateLinkImg"/>
+              </a>
           </Grid>
         </Grid>
       :
