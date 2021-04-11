bed1958e8107fe49bb12bdcd6703e728ad886a40
juslesan <santeri.juslenius@gmail.com>
2021-04-12 01:00:55 +0300

    logos

--- src/pages/project2021.js
+++ src/pages/project2021.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Donate from '../img/project2021/Donate_button.png'
+import Logo1 from '../img/project2021/annette-tillander-logo.svg'
+import Logo2 from '../img/project2021/Tukikummit-logo.png'
+import Logo3 from '../img/project2021/Skatepro_logo_main_2019_black.jpg'
+import Logo4 from '../img/project2021/Grabbarna-flytt-logo-1.png'
+
 export default class Project2020 extends React.Component {
   constructor(props) {
     super(props)
@@ -13,7 +18,7 @@ export default class Project2020 extends React.Component {
     
     return (
       this.props.text ?
-        <Grid container spacing={6} alignItems="top" justify="center">
+        <Grid container spacing={6} alignItems="center" justify="center">
           <Grid item xs={12} sm={12}>
             <h1>Päästä Päähän</h1>
           </Grid>
@@ -23,9 +28,31 @@ export default class Project2020 extends React.Component {
             </div>
           </Grid>
           <Grid item xs={12} sm={12}>
+            <div>
+              <i class="arrow down"></i>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={12}>
             <a href="/construction" className="donateLink">
               <img src={Donate} className="donateLinkImg"/>
             </a>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <div>
+              <p className="project2021text">{this.props.text[1]}</p>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <img className="projectLogo" src={Logo1}/>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <img className="projectLogo" src={Logo2}/>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <img className="projectLogo" src={Logo3}/>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <img className="projectLogo" src={Logo4}/>
           </Grid>
         </Grid>
       :
