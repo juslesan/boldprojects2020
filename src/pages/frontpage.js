4f467e508eb4137a36f723c9a54fdd577c7e6f32
juslesan <santeri.juslenius@gmail.com>
2022-04-23 14:51:53 +0300

    rework frontsize

--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import flames from '../img/long-flames.gif'
 import ukr from '../img/ukr.png'
-
+import logo from '../img/Logo_black.png'
+import { Container } from '@mui/material';
 
 export default class Frontpage extends React.Component {
   constructor(props) {
@@ -10,33 +11,30 @@ export default class Frontpage extends React.Component {
   }
   render() {
     return (
-      <div className="frontPage">
-
-        <Grid container spacing={2} alignItems="center" justifyContent="center" className="frontcont">
-          <Grid item xs={12} sm={6} order={{ xs: 2, sm: 1 }}>
-            <div className="frontGifContainer">
-              <img className="frontGif" src={flames}></img>
-              <p className="frontGifText">
-                Bold Projects is a nonprofit association founded in Helsinki in 2018. With our annual daring endeavors we want to help charities raise awareness and funds. Our projects are eco-friendly, creative and bold.
-              </p>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
-            <div className="frontRightContainer">
-              <img className="frontRightImage" src={ukr}/>
-              <div className="frontRightText">
-                <h1 className="frontFont">Bold Projects</h1>
-                <a href="/project2022" className="frontLink">
-                  <p className="frontLink">Project 2022</p>
-                </a>
-                <a href="https://oma.unicef.fi/en_US/team/bold-projects-three-peaks" target="_blank" className="frontLink">
-                  <p className="frontLink">Donate</p>
-                </a>
-              </div>
-            </div>
-          </Grid>
+      <Grid container spacing={2} alignItems="center" justifyContent="center" className="frontcont">
+        <Grid item xs={12} sm={6} order={{ xs: 2, sm: 1 }} className="frontLeftGrid">
+          <div className="frontGifContainer">
+            {/* <img className="frontGif" src={flames}></img> */}
+            <p className="frontGifText">
+              Bold Projects is a nonprofit association founded in Helsinki in 2018. With our annual daring endeavors we want to help charities raise awareness and funds. Our projects are eco-friendly, creative and bold.
+            </p>
+          </div>
         </Grid>
-      </div>
+        <Grid item xs={12} sm={6} order={{ xs: 1, sm: 2 }}>
+          <div className="frontRightContainer">
+            <img className="frontRightImage" src={ukr}/>
+            <div className="frontRightText">
+              <img className="frontLogo" src={logo}></img>
+              <a href="/project2022" className="frontLink">
+                <p className="frontLink" style={{marginTop: "0"}}>Project 2022</p>
+              </a>
+              <a href="https://oma.unicef.fi/en_US/team/bold-projects-three-peaks" target="_blank" className="frontLink">
+                <p className="frontLink">Donate</p>
+              </a>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     )
   }
 }
--- src/pages/project2022.js
+++ src/pages/project2022.js
@@ -19,29 +19,28 @@ export default class Info extends React.Component {
     console.log(this.props.text)
     return (
       this.props.text ?
-        <Grid container spacing={6} alignItems="center" justifyContent="center">
+        <Grid container spacing={12} alignItems="center" justifyContent="center">
           
           <Grid item xs={12} className="pageHeader">
             <div className='heightLimitDiv'>
               <img className="divBackgroundImage" src={flames}/>
               <div className="overImageHead">
-                <h1 className="project2022header">THREE PEAKS</h1>
+                <h1 className="project2022header" style={{fontFamily: "Recoleta-Bold"}}>THREE PEAKS</h1>
                 <p className="project2022paragraph">{this.props.text[0]}</p>
               </div>
             </div>
           </Grid>
           <Grid>
-            <div className="heightDiv">
               <h1 className="project2022header">UNICEF</h1>
-              <div className="overImage">
+              <div className="unicefBackground">
+                <div className="unicefBackgroundText">
                   <p className="project2022paragraph">{this.props.text[1]}</p>
                   <p className="project2022paragraph">{this.props.text[2]}</p>
+                </div>
               </div>
-              <img className="verticalBackgroundImage" style={{opacity: "60%"}} src={unicef}/>
-            </div>
           </Grid>
           <Grid>
-            <div style={{textAlign: "center", paddingTop: "5%"}}>
+            <div style={{textAlign: "center", paddingTop: "4%"}}>
               <a className="project2022Link" href="https://oma.unicef.fi/en_US/team/bold-projects-three-peaks" target="_blank">
                 <img className="project2022Link" src={donate}></img>
               </a>
