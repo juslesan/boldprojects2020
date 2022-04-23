c4d6c175b381d1602c3da84af041ac8821f608a6
juslesan <santeri.juslenius@gmail.com>
2022-04-24 00:37:12 +0300

    fix build

--- src/pages/construction.js
+++ src/pages/construction.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import map from '../img/club/map.png'
 
 export default class Club extends React.Component {
   constructor(props) {
